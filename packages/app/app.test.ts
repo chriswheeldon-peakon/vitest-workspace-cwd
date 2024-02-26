2999c23c87b77c4f1626c4f4b53b01b57d3411c0
Chris Wheeldon <christopher.wheeldon@workday.com>
2024-02-26 11:20:15 +0000

    working around config

--- packages/app/app.test.ts
+++ packages/app/app.test.ts
@@ -1,7 +1,9 @@
 import path from "node:path";
 
+import config from "config";
 import { expect, test } from "vitest";
 
 test("cwd", () => {
+  expect(config.get("foo")).toEqual("bar");
   expect(path.basename(process.cwd())).toEqual("app");
 });
